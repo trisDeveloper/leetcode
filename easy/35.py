07736b1969236bc42e6bc7cbaf6ea3e24b515aaf
Tris Dev <97057835+trisDeveloper@users.noreply.github.com>
2024-01-07 19:54:21 +0100

    35. Search Insert Position

---
+++ easy/35.py
@@ -0,0 +1,11 @@
+# 35. Search Insert Position
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+         for i in range(len(nums)):
+            if nums[i]==target:
+                return i
+            nums.append(target)
+            nums = sorted(nums)      
+            for i in range(len(nums)):
+                if nums[i]==target:
+                    return i 
